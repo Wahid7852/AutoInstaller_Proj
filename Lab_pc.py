24fa045990bb222e9a0095ed3383c0a3da87d823
Wahid Khan <wahidzk0091@gmail.com>
2025-01-04 23:50:34 +0530

    autoInstaller: improvise logic for client listener
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- Lab_pc.py
+++ Lab_pc.py
@@ -8,7 +8,7 @@ async def ws_server(websocket, path):
         async for message in websocket:
             print(f"Received message: {message}")
             if message == "open_notepad":
-                subprocess.Popen(["notepad.exe"])  
+                subprocess.Popen(["notepad.exe"])
     except websockets.ConnectionClosedError:
         print("Connection closed.")
 
@@ -16,8 +16,5 @@ async def start_ws_server():
     server = await websockets.serve(ws_server, "0.0.0.0", 35369)
     await server.wait_closed()
 
-async def main():
-    await start_ws_server()
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_ws_server())
